e8b5b5440b670a33a51af9f9438347e82a628557
VorsistyiPupok <72183589+VorsistyiPupok@users.noreply.github.com>
2022-09-14 19:30:22 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://f0716659.xsph.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://f0716659.xsph.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/f0716659/domains/f0716659.xsph.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/f0716659/domains/f0716659.xsph.ru/public_html/system/');
+define('DIR_IMAGE', '/home/f0716659/domains/f0716659.xsph.ru/public_html/image/');
+define('DIR_STORAGE', '/home/f0716659/domains/f0716659.xsph.ru/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'f0716659_app_opencart_0');
+define('DB_PASSWORD', 'FwZ7X25IqV');
+define('DB_DATABASE', 'f0716659_app_opencart_0');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
